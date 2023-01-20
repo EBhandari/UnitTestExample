0f2634b6030df1ca86d44745268faff81c61bf52
Ena Bhandari <ena_bhandari2004@yahoo.com>
2023-01-20 13:54:28 +1100

    Code pushed

---
+++ first_test/lib/maths_util.dart
@@ -0,0 +1,15 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int substract(int a, int b) {
+  return a - b;
+}
+
+int multipy(int a, int b) {
+  return a * b;
+}
+
+double divide(int a, int b){
+  return a/b;
+}
